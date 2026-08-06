e43361faadb08a5e6b4c5479ba283e887a733b17
Satoshi Kawasaki <hobbes3@gmail.com>
2026-08-06 11:35:46 +0800

    Added test files again.

---
+++ js/test.js
@@ -0,0 +1,12 @@
+function calculateTotal(price, tax) {
+    // Syntax error: missing closing parenthesis
+    if (price > 0 {
+        return price + (price * tax);
+    }
+
+    // Reference error: 'discount' is undeclared
+    return price - discount;
+}
+
+// Call function with missing arguments
+calculateTotal(;
---
+++ test.md
@@ -0,0 +1,9 @@
+# Project Title
+
+Here is a paragraph that is missing a proper link markdown syntax:
+Check out [Google](https://google.com and missing a closing bracket.
+
+```js
+// Unclosed code block below
+function test() {
+    console.log("broken markdown block");
