062c8d7992f2795ec3c1ae73da957bd91c07be66
Satoshi Kawasaki <hobbes3@gmail.com>
2026-08-18 15:31:42 +0800

    Just testing Lychee ignore now. x8

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,6 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
-    {
-      name: "exclude-woff",
-      generateBundle(_, bundle) {
-        for (const fileName of Object.keys(bundle)) {
-          if (fileName.endsWith(".woff")) {
-            Reflect.deleteProperty(bundle, fileName);
-          }
-        }
-      },
-    },
     viteStaticCopy({
       targets: [
         {
